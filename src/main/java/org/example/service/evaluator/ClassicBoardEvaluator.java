0f949910698b616a3e9d67fd071956946ffd5aab
Yurii Kuznetsov <yurakuznetsov23@gmail.com>
2023-12-02 20:25:41 +0200

    Speed up ClassicBoardEvaluator.evaluate() method

--- src/main/java/org/example/service/evaluator/ClassicBoardEvaluator.java
+++ src/main/java/org/example/service/evaluator/ClassicBoardEvaluator.java
@@ -18,6 +18,12 @@ public class ClassicBoardEvaluator extends BoardEvaluator {
         int scoreO = calculateScore(board, Symbol.O);
 
         return scoreX - scoreO;
+
+//        if (hasSymbolWon(board, Symbol.X)) return calculateScore(board, Symbol.X);
+//        if (hasSymbolWon(board, Symbol.O)) return -calculateScore(board, Symbol.O);
+//        if (board.isFilled()) return 0;
+//
+//        return calculateScore(board, Symbol.X) - calculateScore(board, Symbol.O);
     }
 
     @Override
@@ -30,13 +36,165 @@ public class ClassicBoardEvaluator extends BoardEvaluator {
     }
 
     private int calculateScore(Board board, Symbol symbol) {
-        int lineLength = board.getFormat().getWinLineLength();
-        int score = 0;
+        Symbol[][] matrix = board.getMatrix();
+        int winLineLength = board.getFormat().getWinLineLength();
 
-        for (Symbol[] line : getAllLines(board))
-            score += calculateLineScore(line, symbol, lineLength);
+        return calculateRowsScore(matrix, symbol, winLineLength) +
+                calculateColsScore(matrix, symbol, winLineLength) +
+                calculateMainDiagonalScore(matrix, symbol, winLineLength) +
+                calculateSecondaryDiagonalScore(matrix, symbol, winLineLength);
+    }
 
-        return score;
+    private int calculateRowsScore(Symbol[][] matrix, Symbol symbol, int winLineLength) {
+        int rowsScore = 0;
+
+        for (int i = 0; i < matrix.length; i++) {
+            int rowScore = 0;
+            int symbolCount = 0;
+            int emptyCount = 0;
+
+            for (int j = 0; j < matrix.length; j++) {
+
+                if (matrix[i][j] == symbol) {
+                    symbolCount++;
+                    if (j != matrix.length - 1) continue;
+                }
+                if (matrix[i][j] == Symbol.EMPTY) {
+                    emptyCount++;
+                    if (j != matrix.length - 1) continue;
+                }
+                if (symbolCount + emptyCount >= winLineLength) {
+                    rowScore += getScoreForSymbolsCount(symbolCount);
+                }
+
+                symbolCount = 0;
+                emptyCount = 0;
+            }
+
+            rowsScore += rowScore;
+        }
+
+        return rowsScore;
+    }
+
+    private int calculateColsScore(Symbol[][] matrix, Symbol symbol, int winLineLength) {
+        int colsScore = 0;
+
+        for (int i = 0; i < matrix.length; i++) {
+            int colScore = 0;
+            int symbolCount = 0;
+            int emptyCount = 0;
+
+            for (int j = 0; j < matrix.length; j++) {
+
+                if (matrix[j][i] == symbol) {
+                    symbolCount++;
+                    if (j != matrix.length - 1) continue;
+                }
+                if (matrix[j][i] == Symbol.EMPTY) {
+                    emptyCount++;
+                    if (j != matrix.length - 1) continue;
+                }
+                if (symbolCount + emptyCount >= winLineLength) {
+                    colScore += getScoreForSymbolsCount(symbolCount);
+                }
+
+                symbolCount = 0;
+                emptyCount = 0;
+            }
+
+            colsScore += colScore;
+        }
+
+        return colsScore;
+    }
+
+    private int calculateMainDiagonalScore(Symbol[][] matrix, Symbol symbol, int winLineLength) {
+        int diagonalsCount = 2 * matrix.length - 1;
+        int mainDiagonalsScore = 0;
+
+        for (int i = 0; i < diagonalsCount; i++) {
+            int startRow, startCol;
+            if (i < matrix.length) {
+                startRow = matrix.length - i - 1;
+                startCol = 0;
+            } else {
+                startRow = 0;
+                startCol = i - matrix.length + 1;
+            }
+
+            int diagonalScore = 0;
+            int symbolCount = 0;
+            int emptyCount = 0;
+
+            int length = Math.min(i + 1, diagonalsCount - i);
+            if (length < winLineLength) continue;
+
+            for (int j = 0; j < length; j++) {
+                if (matrix[startRow + j][startCol + j] == symbol) {
+                    symbolCount++;
+                    if (j != length - 1) continue;
+                }
+                if (matrix[startRow + j][startCol + j] == Symbol.EMPTY) {
+                    emptyCount++;
+                    if (j != length - 1) continue;
+                }
+                if (symbolCount + emptyCount >= winLineLength) {
+                    diagonalScore += getScoreForSymbolsCount(symbolCount);
+                }
+
+                symbolCount = 0;
+                emptyCount = 0;
+            }
+
+            mainDiagonalsScore += diagonalScore;
+        }
+
+        return mainDiagonalsScore;
+    }
+
+    private int calculateSecondaryDiagonalScore(Symbol[][] matrix, Symbol symbol, int winLineLength) {
+        int diagonalsCount = 2 * matrix.length - 1;
+        int secondaryDiagonalsScore = 0;
+
+        for (int i = 0; i < diagonalsCount; i++) {
+            int startRow, startCol;
+            if (i < matrix.length) {
+                startRow = i;
+                startCol = 0;
+            } else {
+                startRow = matrix.length - 1;
+                startCol = i - matrix.length + 1;
+            }
+
+            int diagonalScore = 0;
+            int symbolCount = 0;
+            int emptyCount = 0;
+
+            int length = Math.min(i + 1, diagonalsCount - i);
+            if (length < winLineLength) continue;
+
+            for (int j = 0; j < length; j++) {
+                if (matrix[startRow - j][startCol + j] == symbol) {
+                    symbolCount++;
+                    if (j != length - 1) continue;
+                }
+                if (matrix[startRow - j][startCol + j] == Symbol.EMPTY) {
+                    emptyCount++;
+                    if (j != length - 1) continue;
+                }
+                if (symbolCount + emptyCount >= winLineLength) {
+                    diagonalScore += getScoreForSymbolsCount(symbolCount);
+                }
+
+                symbolCount = 0;
+                emptyCount = 0;
+            }
+
+            secondaryDiagonalsScore += diagonalScore;
+        }
+
+        return secondaryDiagonalsScore;
     }
 
     private Symbol[][] getAllLines(Board board) {
